c66b4207f03106e03a84fb3b9db9c568b571a63f
Giannis Panou <giannispanou500@gmail.com>
2025-04-22 19:37:52 +0300

    Mobile Changes

--- js/mobile.js
+++ js/mobile.js
@@ -1,10 +1,13 @@
 const windowSize = window.matchMedia('(max-width: 768px)');
 const header = document.getElementById('header-text');
+const moreBtn = document.getElementById('morebtn');
 
 if (windowSize.matches) {
 	header.classList.remove('flex-row');
 	header.classList.add('flex-column-reverse');
+	moreBtn.classList.add('mt-5');
 } else {
 	header.classList.remove('flex-column-reverse');
 	header.classList.add('flex-row');
+	moreBtn.classList.remove('mt-5');
 }
