988d848c29e5eba99975d39a2bdf5228c451582f
Giannis Panou <giannispanou500@gmail.com>
2025-04-28 15:18:22 +0300

    Layout Changes

--- js/mobile.js
+++ js/mobile.js
@@ -3,16 +3,19 @@ const header = document.getElementById('header-text');
 const introText = document.getElementById('intro-text');
 const introTitle = document.getElementById('intro-title');
 
-if (windowSize.matches) {
-	header.classList.remove('flex-row');
-	header.classList.add('flex-column-reverse');
-	header.classList.remove('align-items-center');
-	introText.classList.add('text-center');
-	introTitle.classList.add('text-center');
-} else {
-	header.classList.remove('flex-column-reverse');
-	header.classList.add('flex-row');
-	header.classList.add('align-items-center');
-	introText.classList.remove('text-center');
-	introTitle.classList.remove('text-center');
+function mobileView() {
+	if (windowSize.matches) {
+		header.classList.remove('flex-row');
+		header.classList.add('flex-column-reverse');
+		introText.classList.add('text-center');
+		introTitle.classList.add('text-center');
+	} else {
+		header.classList.remove('flex-column-reverse');
+		header.classList.add('flex-row');
+		introText.classList.remove('text-center');
+		introTitle.classList.remove('text-center');
+	}
 }
+
+mobileView();
+window.addEventListener('resize', mobileView);
